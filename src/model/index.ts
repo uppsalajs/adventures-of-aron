489067b11f6a889e7b186afd094311a55fd51032
Simon Mika <simon@mika.se>
2025-11-12 14:16:54 +0100

    Added missing exports of model.

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,11 +1,17 @@
 import { Bounds as _Bounds } from "./Bounds"
 import { Direction as _Direction } from "./Direction"
+import { Game as _Game } from "./Game"
+import { Hero as _Hero } from "./Hero"
 import { Point as _Point } from "./Point"
 import { Size as _Size } from "./Size"
+import { Tile as _Tile } from "./Tile"
 
 export namespace model {
 	export import Bounds = _Bounds
 	export import Direction = _Direction
+	export import Game = _Game
+	export import Hero = _Hero
 	export import Point = _Point
 	export import Size = _Size
+	export import Tile = _Tile
 }
