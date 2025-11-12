fb6eebde9219923d8a3560d1f902d5e9106237aa
Simon Mika <simon@mika.se>
2025-11-12 14:18:22 +0100

    Added missing export of Map.

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,6 +2,7 @@ import { Bounds as _Bounds } from "./Bounds"
 import { Direction as _Direction } from "./Direction"
 import { Game as _Game } from "./Game"
 import { Hero as _Hero } from "./Hero"
+import { Map as _Map } from "./Map"
 import { Point as _Point } from "./Point"
 import { Size as _Size } from "./Size"
 import { Tile as _Tile } from "./Tile"
@@ -11,6 +12,7 @@ export namespace model {
 	export import Direction = _Direction
 	export import Game = _Game
 	export import Hero = _Hero
+	export import Map = _Map
 	export import Point = _Point
 	export import Size = _Size
 	export import Tile = _Tile
