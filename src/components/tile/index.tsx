793fd8fdc82b1b9888885aa8d0ad0bd41584cda6
Simon Mika <simon@mika.se>
2025-11-12 14:37:07 +0100

    Rendering basic forest.

--- src/components/tile/index.tsx
+++ src/components/tile/index.tsx
@@ -10,7 +10,11 @@ export class AronTile {
 	@Prop() tile: model.Tile
 	render() {
 		return (
-			<Host>
+			<Host
+				style={{
+					left: (this.tile?.position.x * 64).toString() + "px",
+					top: (this.tile?.position.y * 64).toString() + "px",
+				}}>
 				<img src={`/assets/tiles/${this.tile.type}.svg`} alt="" />
 			</Host>
 		)
